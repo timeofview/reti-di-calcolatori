a862833185b910a22682b472faa5917c44b4c62b
timeofview <neo>
2020-10-09 07:31:21 -0400

    Init server

---
+++ Server/src/RowSwapServer.java
@@ -0,0 +1,14 @@
+
+public class RowSwapServer extends Thread {
+	private String path;
+	private int port;
+	
+	public RowSwapServer(String path, int port) {
+		this.path = path;
+		this.port = port;
+	}
+	
+	public void run() {
+		
+	}
+}
