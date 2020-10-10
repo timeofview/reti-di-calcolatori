15062e42ac90ef3898b476eeda2fd86bb3f692d9
timeofview <neo>
2020-10-10 08:01:07 -0400

    Frabug solution integrated

--- E1/Server/src/DiscoveryServer.java
+++ E1/Server/src/DiscoveryServer.java
@@ -3,7 +3,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.BindException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
--- E1/Server/src/RowSwapServer.java
+++ E1/Server/src/RowSwapServer.java
@@ -1,12 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.net.BindException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RowSwapServer extends Thread {
 	final private int BUFF_LENGTH = 256;
@@ -17,9 +23,9 @@ public class RowSwapServer extends Thread {
 	private int result = -1;
 	private byte[] buff = new byte[BUFF_LENGTH];
 	private ByteArrayInputStream biStream = null;
-	private DataInputStream diStream= null;
-	private int row1,row2=0;
-	private ByteArrayOutputStream boStream= null;
+	private DataInputStream diStream = null;
+	private int row1, row2 = 0;
+	private ByteArrayOutputStream boStream = null;
 	private DataOutputStream doStream = null;
 
 	public RowSwapServer(String path, int port) {
@@ -45,16 +51,70 @@ public class RowSwapServer extends Thread {
 				continue;
 			}
 			try {
-				biStream = new ByteArrayInputStream(packet.getData(),0,packet.getLength());
+				biStream = new ByteArrayInputStream(packet.getData(), 0, packet.getLength());
 				diStream = new DataInputStream(biStream);
 				row1 = diStream.read();
 				row2 = diStream.read();
-			}catch (IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
-				result =-1;
+				result = -1;
 			}
-			/*SWAP*/
-			boStream  = new ByteArrayOutputStream();
+			int result = -1;
+			int currentRow = 0;
+			String c;
+			String rowOne = null;
+			String rowTwo = null;
+			BufferedWriter out;
+			File textOut = null;
+			try {
+				BufferedReader buff = new BufferedReader(new FileReader(path));
+
+				while ((c = buff.readLine()) != null) {
+					currentRow++; // aumento il numero della riga corrente
+					if (currentRow == row1)
+						rowOne = c;
+					else if (currentRow == row2)
+						rowTwo = c;
+				}
+				System.out.print(rowOne + "-" + row1 + "    " + rowTwo + "-" + row2);
+				buff.close();
+
+				buff = new BufferedReader(new FileReader(path));
+				textOut = new File(path + "_temp");
+
+				out = new BufferedWriter(new FileWriter(textOut));
+				c = buff.readLine();
+				currentRow = 0;
+				while (c != null) {
+					currentRow++;
+					if (currentRow == row1) {
+						out.write(rowTwo);
+						out.newLine();
+					} else if (currentRow == row2) {
+						out.write(rowOne);
+						out.newLine();
+					} else {
+						out.write(c);
+						out.newLine();
+					}
+					c = buff.readLine();
+				}
+				out.flush();
+				out.close();
+				buff.close();
+			} catch (IOException e) {
+				System.out.print("File non trovato \n");
+			}
+			try {
+				Files.deleteIfExists(Paths.get(path));
+			} catch (IOException e) {
+				System.out.println("File non rimosso");
+			} // chiudere per non creare problemi con la rename che fallirebbe altrimenti
+
+			textOut.renameTo(new File(path));
+			result = 1;
+
+			boStream = new ByteArrayOutputStream();
 			doStream = new DataOutputStream(boStream);
 			try {
 				doStream.write(result);
