45d18d85b21706e82e4e95a3bfd964a00b14d0f6
timeofview <neo>
2020-10-10 06:58:34 -0400

    Raw hashmap server

--- E1/Server/src/DiscoveryServer.java
+++ E1/Server/src/DiscoveryServer.java
@@ -1,8 +1,75 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.HashMap;
 
 public class DiscoveryServer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final int BUFF_LENGTH = 256;
+		int length = args.length;
+		byte[] buff = new byte[BUFF_LENGTH];
+		DatagramSocket socket = null;
+		DatagramPacket packet = null;
+		HashMap<String, Integer> map = new HashMap<>();
+		if (length % 2 != 1 || length < 3) {
+			System.out.println("Sintssi scortetta.");
+			System.exit(1);
+		}
+		int port = Integer.parseInt(args[0]);
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			e.printStackTrace();
+			System.exit(2);
+		}
+		for (int i = 1; i < length; i += 2) {
+			port = Integer.parseInt(args[i + 1]);
+			if (!map.containsKey(args[i]) && !map.containsValue(port)) {
+				map.put(args[i], port);
+				new RowSwapServer(args[i], port).start();
+			}
+		}
+
+		packet = new DatagramPacket(buff, BUFF_LENGTH);
+		ByteArrayInputStream biStream = null;
+		DataInputStream diStream = null;
+		ByteArrayOutputStream boStream = null;
+		DataOutputStream doStream = null;
+		map.entrySet().stream().forEach(System.out::println);
+		while (true) {
+			packet.setData(buff);
+			try {
+				socket.receive(packet);
+			} catch (IOException e) {
+				e.printStackTrace();
+				continue;
+			}
+			biStream = new ByteArrayInputStream(packet.getData(), 0, packet.getLength());
+			diStream = new DataInputStream(biStream);
+			boStream = new ByteArrayOutputStream();
+			doStream = new DataOutputStream(boStream);
+			try {
+				boStream.write(map.getOrDefault(diStream.readUTF(), -1));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			buff = boStream.toByteArray();
+			packet.setData(buff, 0, buff.length);
+			try {
+				socket.send(packet);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 
--- E1/Server/src/RowSwapServer.java
+++ E1/Server/src/RowSwapServer.java
@@ -1,14 +1,74 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
 public class RowSwapServer extends Thread {
+	final private int BUFF_LENGTH = 256;
 	private String path;
 	private int port;
-	
+	private DatagramSocket socket = null;
+	private DatagramPacket packet = null;
+	private int result = -1;
+	private byte[] buff = new byte[BUFF_LENGTH];
+	private ByteArrayInputStream biStream = null;
+	private DataInputStream diStream= null;
+	private int row1,row2=0;
+	private ByteArrayOutputStream boStream= null;
+	private DataOutputStream doStream = null;
+
 	public RowSwapServer(String path, int port) {
 		this.path = path;
 		this.port = port;
 	}
-	
+
 	public void run() {
-		
+		try {
+			socket = new DatagramSocket(port);
+			packet = new DatagramPacket(buff, buff.length);
+		} catch (SocketException e) {
+			e.printStackTrace();
+			System.exit(2);
+		}
+		while (true) {
+			result = 1;
+			try {
+				packet.setData(buff);
+				socket.receive(packet);
+			} catch (IOException e) {
+				e.printStackTrace();
+				continue;
+			}
+			try {
+				biStream = new ByteArrayInputStream(packet.getData(),0,packet.getLength());
+				diStream = new DataInputStream(biStream);
+				row1 = diStream.read();
+				row2 = diStream.read();
+			}catch (IOException e) {
+				e.printStackTrace();
+				result =-1;
+			}
+			/*SWAP*/
+			boStream  = new ByteArrayOutputStream();
+			doStream = new DataOutputStream(boStream);
+			try {
+				doStream.write(result);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			buff = boStream.toByteArray();
+			packet.setData(buff);
+			try {
+				socket.send(packet);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
