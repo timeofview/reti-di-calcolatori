085cf014f3105b2deca88f671139a7f194733622
Scav <42140533+Scavix@users.noreply.github.com>
2020-10-17 00:27:41 +0200

    Update Client.java

--- E2/Client/src/Client.java
+++ E2/Client/src/Client.java
@@ -6,99 +6,104 @@ import java.nio.file.Paths;
 public class Client {
 	
 	public static void main(String[] args) {
-		//CONTROLLO ARGOMENTI
-		
+
+		// Arguments Check
 		if (args.length != 2) {
 			System.out.println("Usage: Client Address Port");
 			return;
 		}
 		
+		int port = -1;
+		InetAddress addr;
 		
-		
-		int port=-1;
-		InetAddress addr = null;
-		
+		// Arguments Parsing
 		try {
 			 addr = InetAddress.getByName(args[0]);
 			 port = Integer.parseInt(args[1]);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
+		} catch (UnknownHostException uhe) {
+			uhe.printStackTrace();
+			return;
+		} catch (NumberFormatException nfe) {
+			nfe.printStackTrace();
 			return;
 		}
 		
-		
-		Socket socket = null;
+		Socket socket;
 		DataOutputStream dataOutputStream = null;
 		DataInputStream dataInputStream = null;
-		String nameDir = null;
+		String dirName = null;
 		
-		
-		//per ricevere il nome della directory su cui lavorare;
+		// To Read dirName
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 		boolean isDir= false;
+		
+		// Reading and Checks on dirName
 		try {
-			while(isDir==false) {
-				System.out.println("Dichiarare la Directory (EOF per terminare) : ");
-				nameDir = stdIn.readLine();
-				if(nameDir == null) {
-					System.out.println("Selezionato EOF...terminazione");
+			while(isDir == false) {
+				System.out.println("Enter Directory [EOF to end]: ");
+				dirName = stdIn.readLine();
+				if(dirName == null) {
+					System.out.println("Read EOF ... Closing");
 					return;
 				}
-				if(Files.isDirectory(Paths.get(nameDir)))
+				if(Files.isDirectory(Paths.get(dirName))) {
 						isDir=true;
-				else
-					System.out.println("nome o Path passato non corrisponde ad una directory");
-				
-				
+				} else {
+					System.out.println("Path or Name inserted is not a Directory");
+				}
 			}
 			stdIn.close();
 		} catch (IOException e) {
-			
 			e.printStackTrace();
 			return;
 		}
 		
-		//COMUNICAZIONE
-		
+		// Communication
 		try {
 			socket = new Socket(addr, port);
 			socket.setSoTimeout(30000);
-			dataOutputStream=new DataOutputStream(socket.getOutputStream());
-			dataInputStream=new DataInputStream(socket.getInputStream());
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+			dataInputStream = new DataInputStream(socket.getInputStream());
 			int accepted;
-		
-			File[] files = new File(nameDir).listFiles(); //comprende anche le directory
 			BufferedReader buff;
-			String temp;
-			for(File f : files) {
-				if(f.isFile()) {
+			int buffer;
+			
+			// Contain also Directories
+			//File[] files = new File(dirName).list();
+			String[] files = new File(dirName).list();
+			File f;
+			
+			// Main Loop
+			for(String s : files) {
+				if((s.endsWith(".txt"))&&(f = new File(s)).isFile()) {
 					String fname = f.getName();
 					dataOutputStream.writeUTF(fname);
 					dataOutputStream.writeLong(f.length());
-					System.out.println("Attendo risposta dal Server...");
-					if(accepted>=0) {
+					System.out.println("Waing for Server Response...");
 					accepted=dataInputStream.readInt();
+					if(accepted>=0) {
 						buff = new BufferedReader(new FileReader(f));
-							dataOutputStream.writeUTF(temp);
-						while((temp = buff.readLine())!=null) { // da ragionare
+						
+						System.out.println("Reading File and Sending to Server");
+						// Read Until EOF = -1
+						while((buffer = buff.read())>=0) {
+							dataOutputStream.write(buffer);
 						}
 						
 						buff.close();
-					}else {
-						System.out.println("Il server ha rifiutato l'operazione sul file "+f.getName());
+					} else {
+						System.out.println("Server has Refused the Request on File: " + f.getName());
 					}
 				}
 			}
 			
-			
+			//Closing Communications
 			socket.close();
 			dataInputStream.close();
 			dataOutputStream.close();
+			System.out.println("A client has been Served");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
 	}
 }
