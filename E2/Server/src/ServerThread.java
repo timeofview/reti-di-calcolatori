720ecbde17dd397c1123e0469d4d2b3ce46b503f
Scav <42140533+Scavix@users.noreply.github.com>
2020-10-17 20:35:18 +0200

    Update ServerThread.java

--- E2/Server/src/ServerThread.java
+++ E2/Server/src/ServerThread.java
@@ -25,41 +25,44 @@ public class ServerThread extends Thread {
 		try {
 			dataInputStream = new DataInputStream(socket.getInputStream());
 			dataOutputStream = new DataOutputStream(socket.getOutputStream());
-			System.out.println("Thread has been Correctly Initialized");
+			System.out.println("Thread has been Correctly Initialized with: " + Server.usableSpace + " Usable Space");
 			
-			// Reading from Client
-			fileName = dataInputStream.readUTF();
-			dim = dataInputStream.readLong();
-			System.out.println("fileName has been Correctly Read from Client "+fileName);
-			System.out.println("fileDim has been Correctly Read from Client "+dim);
-			
-			// Deciding if Accepting Request
-			if (Files.exists(Paths.get(fileName))||dim>maxDim) {
-				result = -1;
-			} else {
-				result = 1;
+			while ((fileName = dataInputStream.readUTF())!=null) {
+				System.out.println("File Name: "+ fileName + " has been Correctly Read from Client");
+				dim = dataInputStream.readLong();
+				System.out.println("fileDim has been Correctly Read from Client "+dim);	
+				
+				// Deciding if Accepting Request
+				if (Files.exists(Paths.get(fileName))||dim>maxDim||(Server.usableSpace-dim)<0) {
+					result = -1;
+				} else {
+					result = 1;
+				}
+
+				// Sending result
+				dataOutputStream.writeInt(result);
+				System.out.println("Sending result to Client: " + ((result==1) ? "Accepted" : "Refused"));
+
+				// If Everything is fine
+				if (result > 0) {
+					
+					// Receiving the File Byte per Byte
+					outFile = new FileOutputStream(fileName);
+					System.out.println("Receiving from Client and Writing");
+					// Read Until EOF = -1
+					while ((buffer = dataInputStream.read()) > 0) {
+						outFile.write(buffer);
+					}
+					
+					// Closing Communications and Flushing Stream
+					outFile.flush();
+					outFile.close();
+					Server.usableSpace-=dim;
+					System.out.println("A Transfer has been Completed. Usable Space: " + Server.usableSpace);
+				}
 			}
 
-			// Sending result
-			dataOutputStream.writeInt(result);
-			System.out.println("Sending result to Client " + result);
-			
-			// If Everything is fine
-			if (result > 0) {
-				
-				// Receiving the File Byte per Byte
-				outFile = new FileOutputStream(fileName);
-				System.out.println("Receiving from Client and Writing");
-				// Read Until EOF = -1
-				while ((buffer = dataInputStream.read()) >= 0) {
-					outFile.write(buffer);
-				}
-				
-				// Closing Communications and Flushing Stream
-				outFile.flush();
-				outFile.close();
-				System.out.println("A task has been Completed");
-			}
+			socket.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
