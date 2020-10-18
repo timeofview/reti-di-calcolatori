488907199632ec1507021791b4e77e6fed6f8446
alessiotrof <56554383+alessiotrof@users.noreply.github.com>
2020-10-18 22:21:08 +0200

    Fix generali

--- E2/Server/src/Server.java
+++ E2/Server/src/Server.java
@@ -2,58 +2,59 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
-import java.sql.Timestamp;
 
 public class Server {
 
-	// Default Port
-	public static final int PORT = 54321; 
+    // Default timeout and port
+    public static final int DEFAULT_PORT = 54321;
+    private static final int MAX_TIME = 30000;
+    
     public static volatile long usableSpace = 1048576; 
+    
+    public static void main(String[] args) {
+    	
+        int port = -1;
+        
+        // Arguments check
+        try {
+            if (args.length == 1)
+                port = Integer.parseInt(args[0]);
+            else if (args.length == 0)
+                port = DEFAULT_PORT;
+            else {
+                System.out.println("Invalid argument number!\nProgram usage: Server PortNumber[Optional]");
+                System.exit(1);
+            }
+        } catch (NumberFormatException nfe) {
+            nfe.printStackTrace();
+        }
 
-	public static void main(String[] args) {
-		int port = -1;
-		// Arguments Check
-		try {
-			if (args.length == 1)
-				port = Integer.parseInt(args[0]);
-			else if (args.length == 0)
-				port = PORT;
-			else {
-				System.out.println("["+new Timestamp(System.currentTimeMillis())+"] Usage: Server portNumber[optional]");
-				System.exit(1);
-			}
-		} catch (Exception e) {
-			System.out.println("["+new Timestamp(System.currentTimeMillis())+"] Exception during Arguments Check");
-		}
+        ServerSocket serverSocket = null;
+        // Server Socket Creation
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+        } catch (SocketException se) {
+            se.printStackTrace();
+            System.exit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
 
-		ServerSocket serverSocket = null;
+        Socket clientSocket;
+        // Main loop
+        while (true) {
+            try {
+            	
+            	// Accepting client connections 
+                clientSocket = serverSocket.accept();
+                clientSocket.setSoTimeout(MAX_TIME);
 
-		// Server Socket Creation
-		try {
-			serverSocket = new ServerSocket(port);
-			serverSocket.setReuseAddress(true);
-		} catch (SocketException se) {
-			se.printStackTrace();
-			System.exit(1);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(2);
-		}
-		
-		Socket socket;
-		
-		System.out.println("["+new Timestamp(System.currentTimeMillis())+"] Server has been Correctly Initialized");
-		
-		// Main Loop
-		while (true) {
-			try {
-				socket = serverSocket.accept();
-				socket.setSoTimeout(30000);
-				System.out.println("["+new Timestamp(System.currentTimeMillis())+"] Socket has been Correctly Accepted");
-				new ServerThread(socket).start();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+                new ServerThread(clientSocket).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
