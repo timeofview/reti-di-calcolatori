a0d49bf5dc7cb8f66d41459c6ea70f6b465ad236
Scav <42140533+Scavix@users.noreply.github.com>
2020-10-25 15:54:44 +0100

    Update main.c

--- E3/TCP/Client/main.c
+++ E3/TCP/Client/main.c
@@ -2,25 +2,32 @@
 #include <stdlib.h>
 #include <netdb.h>
 #include <fcntl.h>
-#include <zconf.h>
 #include <string.h>
 
-#define DIM_BUFF 5024
+#define DIM_BUFF 1024
 
 int main(int argc,char **argv) {
     int sd,fd_in,fd_out,port,row,nread;
     struct hostent *host;
     char filename[FILENAME_MAX];
     char buff[DIM_BUFF];
+    struct sockaddr_in socket_add;
 
     if(argc!=3){
-        perror("Numero di argomenti sbagliato.");
+        perror("Usage: Client host port");
         exit(1);
     }
     host = gethostbyname(argv[1]);
-    port=atoi(argv[2]);
+    port = atoi(argv[2]);
+    if(host==NULL){
+        perror("NULL host");
+        exit(2);
+    }
 
-    struct sockaddr_in socket_add = {.sin_addr.s_addr= ((struct in_addr *)(host->h_addr)) -> s_addr,.sin_port= port,.sin_family=AF_INET};
+    memset((char *)&socket_add,0,sizeof(struct sockaddr_in));
+    socket_add.sin_family = AF_INET;
+    socket_add.sin_addr.s_addr= ((struct in_addr *)(host->h_addr)) -> s_addr;
+    socket_add.sin_port= htons(port);
 
     printf("Write filename:\n");
     gets(filename);
@@ -28,35 +35,38 @@ int main(int argc,char **argv) {
     scanf("%d",&row);
 
     if((fd_in=open(filename,O_RDONLY))<0){
-        perror("Errore appertura file in input.");
-        exit(2);
-    }
-    if((fd_out=open(filename,O_WRONLY|O_CREAT,0777))<0){
-        perror("Errore appertura file in output.");
+        perror("Error in input file opening");
         exit(3);
     }
-    if(sd = socket(AF_INET,SOCK_STREAM,0)<0){
-        perror("Errore appertura socket.");
+    if((fd_out=open(filename,O_WRONLY|O_CREAT,0777))<0){
+        perror("Error in output file opening");
         exit(4);
     }
-    if(connect(sd,(struct sockaddr *)&socket_add, sizeof(struct sockaddr)) < 0){
-        perror("Errore connessione");
+    if((sd = socket(AF_INET,SOCK_STREAM,0))<0){
+        perror("Error in socket opening");
         exit(5);
     }
+    if((connect(sd,(struct sockaddr *)&socket_add, sizeof(struct sockaddr))) < 0){
+        perror("Error in connection");
+        exit(6);
+    }
     write(sd,&row,sizeof(int));
-
+    printf("Sending\n");
     while((nread=read(fd_in,buff,DIM_BUFF))>0){
         write(sd,buff,nread);
+        printf("%s",buff);
     }
+    printf("\n");
     close(fd_in);
+    printf("Receiving\n");
     while((nread=read(sd,buff,DIM_BUFF))>0){
         write(fd_out,buff,nread);
-        write(0,buff,nread);
+        printf("%s",buff);
     }
     shutdown(sd,2);
     close(sd);
     close(fd_out);
-    printf("f");
+    printf("Transaction End\n");
 
     return 0;
 }
