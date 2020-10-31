4feff14daff3438a641d67ff4c5b07302a358c35
Alessio Troffei <56554383+alessiotrof@users.noreply.github.com>
2020-10-31 21:53:07 +0100

    Aggiunta la mia versione

---
+++ E4/src/clientTCP.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define PORT_MIN 1024
+#define PORT_MAX 65535
+
+/*
+
+	Il secondo Client, chiede ciclicamente all’utente il nome del direttorio, invia al server la
+	richiesta, riceve la lista di nomi di file remoti che stanno nei direttori contenuti nel direttorio
+	specificato,e la stampa a video
+
+*/
+
+
+#define STR_MAX 256
+
+
+int main(int argc, char * argv[]) {
+
+
+    int sd, nRead, port, i;
+    char dirName[STR_MAX], serverReply[STR_MAX];
+    struct sockaddr_in serverAddr;
+    struct hostent *serverHost;
+    struct timeval timeout; // Struct timeout, utilizzata per interrompere la connessione se scade
+    timeout.tv_sec = 30;
+    timeout.tv_usec = 0;
+
+
+
+    // Controllo argomenti
+    if(argc != 3) {
+        fprintf(stderr, "Error: invalid argument number!\nProgram usage: %s Address Port", argv[0]);
+
+        exit(EXIT_FAILURE);
+    }
+
+
+    // Controllo che l'argomento "Port" sia composto da soli numeri
+    for(i=0; i<strlen(argv[2]); i++) {
+        if (argv[2][i] < '0' || argv[2][i] > '9') {
+            fprintf(stderr, "Error: port argument '%s' is not an only-digit!\n", argv[2]);
+
+            exit(EXIT_FAILURE);
+        }
+    }
+
+
+    // Controllo che l'argomento "Port" non ecceda il limite massimo di 65K
+    // Converto la porta in intero e controllo che non super il limite
+    port = atoi(argv[2]);
+    if(port < PORT_MIN || port > PORT_MAX) {
+        fprintf(stderr, "Error: port is exceeding the 65K limit!\n");
+    }
+
+    serverHost = gethostbyname(argv[1]);
+    if(serverHost == NULL) {
+        perror("Error unknown Server: ");
+
+    }
+
+
+    // Setto a 0 il contenuto della variabile serverAddr
+    memset((char*) &serverAddr, 0, sizeof(struct sockaddr_in));
+
+    // Setto i parametri di indirizzo Socket
+    serverAddr.sin_family = AF_INET; // Famiglia protocolli Internet
+    serverAddr.sin_addr.s_addr = ((struct in_addr*) (serverHost->h_addr))->s_addr; // Ottengo l'indirizzo
+    serverAddr.sin_port = htons(port); // Assegno la parta in base a ciò che l'utente mi ha inserito
+
+
+    // Apertura Socket
+    if((sd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+        perror("Socket error: ");
+
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    // Setto il tempo di ricezione nelle opzioni della Socket
+    if (setsockopt (sd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(struct timeval)) < 0) {
+        fprintf(stderr, "Error: setsockopt failed\n");
+    }
+    if (setsockopt (sd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(struct timeval)) < 0) {
+        fprintf(stderr, "Error: setsockopt failed\n");
+    }
+
+
+    // Connect
+    if(connect(sd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr)) < 0) {
+        perror("Connection error: ");
+
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    // Chiedo ciclicamente all'untente il nome del direttorio
+    printf("Enter directory name [EOF to end]: ");
+    while(scanf("%s", dirName) != EOF) {
+
+        // Invio della stringa
+        if(send(sd, dirName, strlen(dirName), 0) < 0) {
+            perror("Send failed: ");
+
+            continue;
+        }
+
+        // Ricevo la lista delle Stringhe
+        while((nRead = recv(sd, serverReply, STR_MAX * sizeof(char), 0)) > 0) {
+
+            // Errore recv
+            if(nRead < 0) {
+                perror("Recv error: ");
+
+                continue;
+            }
+
+            printf("%s\n", serverReply);
+
+            // Flusho la stringa
+            memset((char*) &serverReply, 0, STR_MAX * sizeof(char));
+        } // fine while recv
+
+        printf("Enter directory name [EOF to end]: ");
+    } // fine while
+
+
+    printf("Ending communication\n");
+
+    // Chiusura comunicazione
+    shutdown(sd, SHUT_RDWR);
+    close(sd);
+
+    return 0;
+}
