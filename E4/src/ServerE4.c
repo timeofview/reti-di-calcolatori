8c30b9b707a5b074cc1cfc560e419aa9efa54736
Scav <Scav>
2020-11-03 00:06:08 +0100

    Corrected Segmentation Fault

--- E4/src/ServerE4.c
+++ E4/src/ServerE4.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]) {
 
         // Controllo che l'argomento "Port" sia composto da soli numeri
         for(i=0; i<strlen(argv[1]); i++) {
-            if (argv[2][i] < '0' || argv[1][i] > '9') {
+            if (argv[1][i] < '0' || argv[1][i] > '9') {
                 fprintf(stderr, "Error: port argument '%s' is not an only-digit!\n", argv[2]);
 
                 exit(EXIT_FAILURE);
